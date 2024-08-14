aa1f5a15bc4b70426af0745726e8e3783ca85dc6
caesura08 <jasmcknight233@gmail.com>
2024-08-13 19:25:23 -0700

    added the html for calculator along with linking the buttons to the script

---
+++ script.js
@@ -0,0 +1,45 @@
+const one = document.getElementById('btn1');
+const two = document.getElementById('btn2');
+const three = document.getElementById('btn3');
+const four = document.getElementById('btn4');
+const five = document.getElementById('btn5');
+const six = document.getElementById('btn6');
+const seven = document.getElementById('btn7');
+const eight = document.getElementById('btn8');
+const nine = document.getElementById('btn9');
+const zero = document.getElementById('btn0');
+const equal = document.getElementById('btnEqual');
+const sub = document.getElementById('btnSub');
+const add = document.getElementById('btnAdd');
+const mult = document.getElementById('btnMul');
+const divi = document.getElementById('btnDiv');
+const deci = document.getElementById('btnDeci');
+const display = document.getElementById('display');
+
+function add (a, b) { ;
+
+  a + b;
+}
+
+function subtract (a, b) {
+    a - b;
+}
+
+function multiply (a, b) {
+    a * b;
+}
+
+function divide (a, b) {
+    a / b;
+}
+
+let num1 = '';
+let operator = '';
+let num2 = '';
+
+let operate = function() {
+    
+}
+
+let currentDisplay = 0;
+let resultDisplay = false;
